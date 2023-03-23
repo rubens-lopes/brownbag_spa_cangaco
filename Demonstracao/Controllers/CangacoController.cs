bd4286abca5288bf6d914949aec51581a4088aae
Rubens Lopes <rubens.lopes@lambda3.com.br>
2023-03-23 17:22:24 -0400

    wip: tailwind + prototipo

---
+++ Demonstracao/Controllers/CangacoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpaCangaço.Demonstração.Controllers;
+
+public class CangacoController : Controller
+{
+	public IActionResult Index()
+	{
+		return View();
+	}
+}
