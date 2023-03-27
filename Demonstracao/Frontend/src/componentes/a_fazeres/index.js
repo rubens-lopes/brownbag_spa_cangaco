cdc40ae2c46f7f8c80a4cad284b23ce35cc95a3e
Rubens Armôa Lopes <github@rubenslop.es>
2023-03-27 15:52:27 -0400

    wip: começa a implementar funcionalidade

--- Demonstracao/Frontend/src/componentes/a_fazeres/index.js
+++ Demonstracao/Frontend/src/componentes/a_fazeres/index.js
@@ -1,14 +1,18 @@
 import Componente from '&/motor/componente'
 import Visualização from './visualizacao'
 import './estilos.sass'
-import Cabeçalho from '../cabecalho'
+import Cabeçalho from '&/componentes/cabecalho'
+import Formulário from '&/componentes/formulario_tarefa'
+import Lista_De_Tarefas from '&/componentes/lista_de_tarefas'
 
 export default class A_Fazeres extends Componente {
-	constructor() {
-		super(new Visualização(`[data-a-fazeres-bb]`), {})
+	constructor({ tarefas }) {
+		super(new Visualização(`[data-a-fazeres-bb]`), { tarefas })
 	}
 
-	hidratar({ modelo, visualização }) {
+	hidratar({ modelo }) {
 		new Cabeçalho({ valor: `A fazeres`})
+		new Formulário({ ao_submeter: (a) => console.log(a)})
+		new Lista_De_Tarefas({ tarefas: modelo.tarefas})
 	}
 }
--- Demonstracao/Frontend/src/componentes/a_fazeres/visualizacao.js
+++ Demonstracao/Frontend/src/componentes/a_fazeres/visualizacao.js
@@ -1,9 +1,13 @@
 import Visualização from '&/motor/visualizacao'
 
 export default class Visualização_A_Fazeres extends Visualização {
-	obter_visualização({ lista }) {
+	obter_visualização() {
 		return `
 			<div data-cabeçalho-bb></div>
+
+			<div data-formulário-tarefa-bb></div>
+
+			<div data-lista-de-tarefas-bb></div>
 		`
 	}
 }
